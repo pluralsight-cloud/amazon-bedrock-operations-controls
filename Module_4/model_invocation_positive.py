41d86a81528ef29a3f3f77a3e212ed776d0f6e23
Biswanath Halder <bnhalder@Biswanaths-MacBook-Air.local>
2026-03-07 20:35:18 +0530

    Positive and negative examples for module 4

---
+++ Module_4/model_invocation_positive.py
@@ -0,0 +1,50 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+
+    client = boto3.client("bedrock-runtime")
+
+    prompt = """
+    Can you tell me what is Amazon Bedrock in one sentence?
+    """
+
+    body = {
+        "schemaVersion": "messages-v1",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "text": prompt
+                    }
+                ]
+            }
+        ],
+        "inferenceConfig": {
+            "maxTokens": 300,
+            "temperature": 0.2
+        }
+    }
+
+    response = client.invoke_model(
+        modelId="amazon.nova-lite-v1:0",
+        body=json.dumps(body),
+        guardrailIdentifier="ah3ph7nat50x",
+        guardrailVersion="1",
+        contentType="application/json",
+        accept="application/json"
+    )
+
+    response_body = json.loads(response["body"].read())
+
+    generated_text = response_body["output"]["message"]["content"][0]["text"]
+    guardrail_action = response_body.get("amazon-bedrock-guardrailAction")
+
+    print("Guardrail Action:", guardrail_action)
+    print("Response:", generated_text)
+
+    return {
+        "statusCode": 200,
+        "body": generated_text
+    }
